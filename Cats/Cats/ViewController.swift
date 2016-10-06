8d5060d3df446e87bc57ca6bc6fd2c756e7ed1b2
Paul Tangen <ptangen@ptangen.com>
2016-10-06 13:42:33 -0400

    Complete the tableview cats lab.

--- Cats/Cats/ViewController.swift
+++ Cats/Cats/ViewController.swift
@@ -10,8 +10,25 @@ import UIKit
 
 class ViewController: UITableViewController {
     
+    @IBOutlet var tableView1: UITableView!
     
+    let myArray = ["Abyssinian", "American Shorthair", "Bengal", "Birman", "British Shorthair",
+    "Burmese", "Exotic Shorthair", "Maine Coon", "Norwegian Forest", "Persian", "Ragdoll"]
    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "protoTypeCell", for: indexPath)
+        let itemTitle = myArray[indexPath.row]
+        cell.textLabel?.text = itemTitle // its ok to force unwrap a UI element
+        return cell
+    }
     
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myArray.count
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     
 }
